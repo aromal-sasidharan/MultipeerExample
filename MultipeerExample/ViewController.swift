b9e7e42bb977d99e4a4efe40272f5c8998d18db8
aromalsasidharan <aromalsasidharan@gmail.com>
2015-10-05 14:33:38 +0530

    Initial Commit

---
+++ MultipeerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipeerExample
+//
+//  Created by Aromal Sasidharan on 10/5/15.
+//  Copyright © 2015 Aromal Sasidharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
